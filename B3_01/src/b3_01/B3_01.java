8b6f2a4d8459a1f929a27a51a1b83d811fd936f2
Rubén Carrera Amoedo <rcarreraamoedo@victoria11.ad.danielcastelao.org>
2019-10-09 14:22:58 +0200

    modificado ej1

--- B3_01/src/b3_01/B3_01.java
+++ B3_01/src/b3_01/B3_01.java
@@ -15,7 +15,8 @@ public class B3_01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Modificación
+        System.out.println("Hola ");
     }
     
 }
